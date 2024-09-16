a239bcd1217529fd490647127b9003efede2ce0c
Howard Huang <howard.huang3+git@gmail.com>
2024-09-16 10:39:09 -0400

    Create blank MenuListing and RestaurantListing Components

---
+++ src/components/MenuListing/MenuListing.jsx
@@ -0,0 +1,18 @@
+import './MenuListing.scss'
+
+function MenuListing() {
+  return (
+    <>
+        <div className="menu-listing">
+            <div className="menu-listing__left">
+                
+            </div>
+            <div className="menu-listing__right">
+                
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default MenuListing
