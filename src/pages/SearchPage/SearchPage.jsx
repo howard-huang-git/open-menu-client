b0e06816541e1df25fd8d9fcdde0ad53c99358bb
Howard Huang <howard.huang3+git@gmail.com>
2024-09-17 21:10:29 -0400

    Add Search Banner

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -13,7 +13,9 @@ function SearchPage() {
   const [menuData, setMenuData] = useState([]);
   const [restaurantData, setRestaurantData] = useState([]);
   const [searchFilter, setSearchFilter] = useState({search: "", area: "", sort: ""});
+  const [searchType, setSearchType] = useState(true);
 
+  // API Calls
   const fetchMenu = async () => {
     try {
       let response = await axios.get(apiUrl + "/foods");
@@ -45,14 +47,26 @@ function SearchPage() {
     fetchRestaurants();
   }, []);
 
+  // Search Parameter Functions
   const sortFunction = (sort) => {
-    if (sort === "price-sort") {
-      return function(a, b){return a.price - b.price}
-    } else if (sort === "rating-sort") {
-      return function(a, b){return b.avg_rating - a.avg_rating}
-    } else {
-      return function(a, b){return b - a}
+    if (searchType === true) {
+      if (sort === "price-sort") {
+        return function(a, b){return a.price - b.price}
+      } else if (sort === "rating-sort") {
+        return function(a, b){return b.avg_rating - a.avg_rating}
+      } else {
+        return function(a, b){return b - a}
+      }
+    } else if (searchType === false) {
+      if (sort === "price-sort") {
+        return function(a, b){return (a.max + a.min)/2 - (b.max + b.min)/2}
+      } else if (sort === "rating-sort") {
+        return function(a, b){return b.rating - a.rating}
+      } else {
+        return function(a, b){return b - a}
+      }
     }
+    
   }
 
   const areaFunction = (area) => {
@@ -63,12 +77,45 @@ function SearchPage() {
     if (search.trim() === "") {
       return (item) => item
     } else {
-      return (item) => item.item.toUpperCase().includes(search.trim().toUpperCase()) || item.category.toUpperCase().includes(search.trim().toUpperCase())
+      let searchTerm = search.trim().toUpperCase()
+      if (searchType === true) {
+        return (item) => item.item.toUpperCase().includes(searchTerm) || item.category.toUpperCase().includes(searchTerm) 
+      } else if (searchType === false) {
+        return (item) => item.name.toUpperCase().includes(searchTerm) || item.type.toUpperCase().includes(searchTerm)
+      }
+      
     }
   }
 
-  const searchType = true;
+  const toggleMenuSearch = () => {
+    if (searchType === false) {
+      setSearchType(true)
+    }
+  }
 
+  const toggleRestaurantSearch = () => {
+    if (searchType === true) {
+      setSearchType(false)
+    }
+  }
+
+  const classMenuSearch = () => {
+    if (searchType === true) {
+      return "search-banner--active"
+    } else {
+      return "search-banner--inactive"
+    }
+  }
+
+  const classRestaurantSearch = () => {
+    if (searchType === false) {
+      return "search-banner--active"
+    } else {
+      return "search-banner--inactive"
+    }
+  }
+
+  // Components
   const searchList = (type) => {
     if (type === true) {
       return (
@@ -88,7 +135,7 @@ function SearchPage() {
       return (
         <>
           {
-            restaurantData.map((item) => (
+            restaurantData.filter(searchFunction(searchFilter.search)).filter(areaFunction(searchFilter.area)).sort(sortFunction(searchFilter.sort)).map((item) => (
               <div key={item.id} className="search-page__listing">
                 <RestaurantListing key={item.id} data={item} />
                 <Divider />
@@ -103,6 +150,10 @@ function SearchPage() {
 
   return (
     <>
+      <div className="search-banner">
+        <div className={`search-banner__food ` + classMenuSearch()} onClick={toggleMenuSearch}>Menu Item</div>
+        <div className={`search-banner__restaurant ` + classRestaurantSearch()} onClick={toggleRestaurantSearch}>Restaurant</div>
+      </div>
       <SearchBox change={setSearchFilter} />
       <Divider />
       {searchList(searchType)}
